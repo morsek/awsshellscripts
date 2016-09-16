1f5728c126257aca4b2b2475abc310c5c9966831
Kristen Morse <morsekl20@mounties.mansfield.edu>
2016-09-16 00:00:42 +0000

    AWS Web Server Setup Script

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# by Kristen Morse 20160915
+# run as root; chmod 700 setup.sh; ./setup.sh
+
+date 
+
+apt-get -qq update -y
+apt-get -qq upgrade -y
+
+hostnamectl set-hostname localhost
+hostnamectl
+
+apt-get -qq install -y zip unzip
+
+apt-get -qq install -y apache2
+systemctl restart apache2
+
+date
+
